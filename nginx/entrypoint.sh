3d1cc113c94ef9afe1d64aba97688be6659c968d
Stanislav Vitko <vist.rcm@gmail.com>
2017-05-04 23:08:00 -0700

    dh for nginx

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# gen dh
+if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
+    echo "make dhparams"
+    cd /etc/nginx/ssl
+    openssl dhparam -out dhparams.pem 2048
+    chmod 600 dhparams.pem
+fi
+
+# start nginx
+nginx -g daemon off
