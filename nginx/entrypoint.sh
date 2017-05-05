8bd5ee2ac8632a3409c9fccbaff116c638babc13
Stanislav Vitko <vist.rcm@gmail.com>
2017-05-04 23:11:30 -0700

    dh for nginx. fix directory

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,6 +3,7 @@
 # gen dh
 if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
     echo "make dhparams"
+    mkdir -p /etc/nginx/ssl
     cd /etc/nginx/ssl
     openssl dhparam -out dhparams.pem 2048
     chmod 600 dhparams.pem
